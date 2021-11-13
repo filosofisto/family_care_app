2bb682c397dcf7e9bacc8ae810298211fc5460ef
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2021-11-13 22:13:46 +0100

    infoIcon method

--- lib/components/instant_message.dart
+++ lib/components/instant_message.dart
@@ -53,7 +53,45 @@ class InstantMessage {
             Text(
               message,
               style: TextStyle(fontSize: 16.0),
-            )
+            ),
+          ],
+        ),
+      ),
+      duration: const Duration(milliseconds: 3000),
+      width: MediaQuery.of(context).size.width * 0.9,
+      backgroundColor: Colors.lightBlueAccent,
+      // Width of the SnackBar.
+      padding: const EdgeInsets.symmetric(
+        horizontal: 8.0, // Inner padding for SnackBar content.
+      ),
+      behavior: SnackBarBehavior.floating,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    ));
+  }
+
+  static void infoIcon(BuildContext context, String message, Icon icon) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Padding(
+        padding: const EdgeInsets.only(top: 16.0, bottom: 16.0),
+        child: Row(
+          children: [
+            Icon(
+              FontAwesomeIcons.info,
+              color: Colors.white,
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Text(
+              message,
+              style: TextStyle(fontSize: 16.0),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            icon
           ],
         ),
       ),
